dd19273c326163328d49d7c658b7e93ef5476677
sravani7898 <sravanibadugu7@gmail.com>
2021-06-07 15:58:35 +0530

    exception

---
+++ exceptiondemosol/exceptiondemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptiondemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1 :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number2 :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+            try
+            {
+                int res = num1 / num2;
+                Console.WriteLine("the result is " + res);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine("Application Completed");
+            }
+         
+            finally
+            {
+                Console.WriteLine("Application Completed");
+            }
+            Console.ReadLine();
+        }
+    }
+}
